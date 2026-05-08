ce04728b90586f8a42740e672e50fd86020aae24
fabricio-sant <011fabricio27@gmail.com>
2026-05-08 14:26:57 -0300

    criamos um decremento

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,6 +1,15 @@
-function minhaFuncao(){
-    alert("Minha Fnção")
-    console.log("Sou Dev e você?")
-}
+let valor = 12;
 
-minhaFuncao();
+console.log("agora vale: ", valor);
+
+valor = valor - 1
+
+console.log("valor");
+
+valor--;
+
+console.log("agora vale: ", valor);
+
+valor -= 4
+
+console.log("agora vale: ", valor);
