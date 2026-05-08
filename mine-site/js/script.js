7f53200f8c19e08647ac486e0437ab7e277cdf19
fabricio-sant <011fabricio27@gmail.com>
2026-05-08 15:42:29 -0300

    criei um laço de repetição com o nome suppinpa

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,3 +1,14 @@
-for (let i = 0; i <=5; i++) {
-    console.log("O valor de i, vale " + i)
+/*
+  - Escreva um loop (for) que exiba a frase abaixo
+   no console;
+  - Substitua o "X" pela informação correta;
+  - "X" deve começar em 1 na 1ª exibição da frase e em 
+  10 na última;
+  - Exiba a frase 10x no console.
+
+  "Esta é a Xª vez que esta frase é exibida no console."
+*/
+
+for (let suppinpa = 1; suppinpa <=10; suppinpa++) {
+    console.log('Esta é o ${suppinpa}ª vez que esta frase é exibida no console');
 }
