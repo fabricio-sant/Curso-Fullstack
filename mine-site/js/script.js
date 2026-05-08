450c76a473dac03df0eb710a7644103e9bc7e161
fabricio-sant <011fabricio27@gmail.com>
2026-05-08 14:41:12 -0300

    criei um laço de repetição (for)

--- mine-site/js/script.js
+++ mine-site/js/script.js
@@ -1,15 +1,3 @@
-let valor = 12;
-
-console.log("agora vale: ", valor);
-
-valor = valor - 1
-
-console.log("valor");
-
-valor--;
-
-console.log("agora vale: ", valor);
-
-valor -= 4
-
-console.log("agora vale: ", valor);
+for (let i = 0; i <=5; i++) {
+    console.log("O valor de i, vale " + i)
+}
