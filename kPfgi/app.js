7cea5efce1ce0c95feaaaaeeb49e9e48f18fab3f
fabricio-sant <011fabricio27@gmail.com>
2026-05-19 14:55:29 -0300

    mexendo no insomnia

--- kPfgi/app.js
+++ kPfgi/app.js
@@ -2,13 +2,43 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+//Variavel com os dados
+const alimentos = [
+    {id: 1, nome: "Feijão", preco: 10},
+    {id: 2, nome: "Carne", preco: 50},
+    {id: 3, nome: "Frutas", preco: 70},
+    {id: 4, nome: "Sopas", preco: 40}
+];
+
+const tiposPratos = [
+    {id: 1, nome: "Entrada"},
+    {id: 2, nome: "Prato Principal"},
+    {id: 3, nome: "Sobremesa"},
+    {id: 4, nome: "Prato Vegetariano"}
+];
+
 //Criar uma função
 //rota principal
 app.get('/', (req, res) => {
     res.send("Bem vindo ao meu projeto de Alimentos")
 });
 
+//Tipos de Pratos
+app.get('/TiposPratos', (req, res) => {
+    res.send(tiposPratos);
+});
+
+//Produtos
+app.get('/Produtos', (req, res) => {
+    res.send(alimentos);
+});
+
+//Tipos de Pratos
+app.get('/TiposPratos', (req, res) => {
+    res.send(tiposPratos);
+});
+
 //executando o servidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Servidor do projeto,rodando em http://localhost:${port}`);
 });
